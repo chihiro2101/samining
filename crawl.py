535821c47d1cd17721dc64dc75ceb55122f38975
ICSME2023-DebtSniffer <bannhomeocon@gmail.com>
2024-03-15 12:30:16 +0100

    fix code

--- crawl.py
+++ crawl.py
@@ -6,16 +6,12 @@ import base64
 # Function to save README content to a text file
 def save_readme_to_file(repo_name, readme_content):
     # Creating directory if not exists
-    if not os.path.exists('readmes'):
-        os.makedirs('readmes')
-
-    # # Writing README content to a text file
-    # with open(f'readmes/{repo_name}_README.txt', 'w', encoding='utf-8') as file:
-    #     file.write(readme_content)
+    if not os.path.exists('readmefiles'):
+        os.makedirs('readmefiles')
 
 
     # Writing content to file
-    with open(f'readmes/{repo_name}_README.txt', 'wb') as file:
+    with open(f'readmefiles/{repo_name}_README.txt', 'wb') as file:
         file.write(readme_content)
 
 def download_file_from_github(url, file_path):
@@ -28,8 +24,6 @@ def download_file_from_github(url, file_path):
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
     response = requests.get(api_url)
 
-    # import pdb;pdb.set_trace()
-    
     if response.status_code == 404:
         file_path = "README.rst"
         api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}"
@@ -43,7 +37,6 @@ def download_file_from_github(url, file_path):
             # Decoding content from base64
             file_content = content['content'].encode('utf-8')
             file_content = base64.b64decode(file_content)
-            # import pdb;pdb.set_trace()
             save_readme_to_file(repo_name, file_content)
             print(f"File '{repo_name}/{file_path}' downloaded successfully.")
         else:
@@ -58,7 +51,6 @@ with open(input_file_name, 'r') as f:
     urls = f.readlines()
 
 
-# Loop through each URL, fetch README content and save to a file
 for url in urls:
     github_url = url.split(",")[0].strip("\"")
     file_path = "README.md"
